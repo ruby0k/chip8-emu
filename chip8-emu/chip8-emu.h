fefc3e142d42a1ac2c95286f189491d13d68e507
Illia <zakharovillia0@gmail.com>
2023-12-08 23:39:35 +0100

    Init

---
+++ chip8-emu/chip8-emu.h
@@ -0,0 +1,11 @@
+﻿// chip8-emu.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#define SDL_MAIN_HANDLED
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+#include <SDL.h>
